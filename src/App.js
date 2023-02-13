418dcfc5333f5f140dd70bd56e9d5c5e194be526
fitz95 <fitzgeraldnsong@gmail.com>
2023-02-13 14:03:24 +0100

    App name change

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Math Magician App
         </a>
       </header>
     </div>
