918446c74f8de553f6d2db2216adbf0874851f11
fitz95 <fitzgeraldnsong@gmail.com>
2023-02-23 06:10:42 +0100

    adding routes

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+import { GiHamburgerMenu } from 'react-icons/gi';
+
+function Navbar() {
+  return (
+    <>
+      <header className="site-header" style={{ color: 'black' }}>
+        <div className="wrapper site-header__wrapper">
+          <h1 className="brand"><Link to="/" style={{ textDecoration: 'none' }}>MATH MAGICIANS</Link></h1>
+          <nav className="nav">
+            {/* <button className="nav__toggle" aria-expanded="false" type="button">
+              menu
+            </button> */}
+            <GiHamburgerMenu className="nav__toggle" aria-expanded="false" />
+            <ul className="nav__wrapper">
+              <li className="nav__item">
+                <Link to="/" style={{ textDecoration: 'none' }}>Home</Link>
+              </li>
+
+              <li className="nav__item">
+                <Link to="/Calculator" style={{ textDecoration: 'none' }}>Calculator</Link>
+              </li>
+              <li className="nav__item">
+                <Link to="/Quote" style={{ textDecoration: 'none' }}>Quote</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+    </>
+  );
+}
+export default Navbar;
---
+++ src/routes/Quote.jsx
@@ -0,0 +1,19 @@
+import Navbar from '../components/Navbar';
+
+function Quote() {
+  return (
+    <>
+      <Navbar />
+      <div className="quotediv">
+        <h1>
+          Mathematics is not about numbers, equations, computations, or
+          algorithms: It is about understanding
+          <br />
+          - William Paul Thurson
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default Quote;
